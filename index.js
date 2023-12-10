ee4f617073d37e0b8ebce4d0612ee06601a3e53a
vikram7375 <145310330+vikram7375@users.noreply.github.com>
2023-12-10 20:07:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "delta_app",
+    password: "viksaa@123",
+  });
+try{connection.query("SHOW TABLES",(err, result)=>{
+    if(err)throw err;
+    console.log(result);
+  });
+}catch (err) {
+    console.log(err);
+}
+
+let getRandomUser = () => {
+    return {
+      id: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+     
+    };
+  };
